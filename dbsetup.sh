c22e9006ebe08d1748041c339ff28ea67957543f
Per Antoine Carlsen <per.carlsen@oda.com>
2021-09-06 23:37:11 +0200

    Fix parameter

--- dbsetup.sh
+++ dbsetup.sh
@@ -1,11 +1,12 @@
 while getopts "d:" flag
 do
     case "${flag}" in
-        d) database="${OPTARG:-shopify}";;
+        d) database="${OPTARG}";;
     esac
 done
-createdb "$database" "Shopify customer, inventory, product and order database" --owner="$USER" --username="$USER"
 
-psql -d "$database" -U "$USER" -f ./setup.sql
+createdb "${database:-shopify}" "Shopify customer, inventory, product and order database" --owner="$USER" --username="$USER"
+
+psql -d "${database:-shopify}" -U "$USER" -f ./setup.sql
 
 echo "Database setup completed"
